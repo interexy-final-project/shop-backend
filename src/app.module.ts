d61c3dc5d0bc0cedbd9e3b5583525133d7805ce7
Toni <aiming1high@gmail.com>
2023-08-23 18:14:23 +0300

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+// ========== config ==========
+import app_config from './config/app.config';
+import database_config from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [app_config, database_config],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
