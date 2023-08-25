b069a66f56fa8df83e34498b8e503b342b71502e
Irina Dubrovskaya <irina.dubrovskaya95@gmail.com>
2023-08-25 12:41:52 +0300

    feat: added jwt-permissions guard

---
+++ src/app/security/dtos/user-session.dto.ts
@@ -0,0 +1,40 @@
+import { EUserPermissions } from 'app/user-roles/enums/user-permissions.enum';
+import { UserEntity } from 'app/users/entities/user.entity';
+import { IsUUID, IsString, IsNumber, IsArray } from 'class-validator';
+
+export class UserSessionDto {
+  @IsUUID()
+  id: string;
+
+  @IsString()
+  email: string;
+
+  @IsNumber()
+  role_id: number;
+
+  @IsArray({ context: EUserPermissions })
+  permissions: EUserPermissions[];
+
+  constructor() {}
+
+  public static from(dto: UserSessionDto): UserSessionDto {
+    return {
+      id: dto.id,
+      email: dto.email,
+      role_id: dto.role_id,
+      permissions: dto.permissions,
+    };
+  }
+
+  public static fromEntity(
+    entity: UserEntity,
+    permissions: EUserPermissions[],
+  ): UserSessionDto {
+    return {
+      id: entity.id,
+      email: entity.email,
+      role_id: entity.roleId,
+      permissions,
+    };
+  }
+}
