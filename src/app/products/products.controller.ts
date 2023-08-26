acfa0bba838f3303cb451979a864f991646614c1
Toni <aiming1high@gmail.com>
2023-08-26 23:35:50 +0300

    added endpoint to get products by category

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductDto } from './dto/product.dto';
+import { PaginationQueryDto } from 'shared/dtos/pagination-query.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -9,6 +10,20 @@ export class ProductsController {
   public async getProducts() {
     const entities = await this.productsService.getAllProducts();
     const products = ProductDto.fromEntities(entities);
+    return products || [];
+  }
+
+  @Get(':category')
+  public async getByCategory(
+    @Param('category') category: string,
+    @Query() paginationQuery: PaginationQueryDto,
+  ) {
+    const entitiesAndCount =
+      await this.productsService.getAllProductsByCategory(
+        category,
+        paginationQuery,
+      );
+    const products = ProductDto.fromEntities(entitiesAndCount[0]);
     return products || [];
   }
 }
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsRepo } from './repo/products.repo';
+import { PaginationQueryDto } from 'shared/dtos/pagination-query.dto';
 
 @Injectable()
 export class ProductsService {
@@ -7,5 +8,12 @@ export class ProductsService {
 
   public async getAllProducts() {
     return this.repo_products.getAll();
+  }
+
+  public async getAllProductsByCategory(
+    category: string,
+    paginationQuery: PaginationQueryDto,
+  ) {
+    return this.repo_products.getAllByCategory(category, paginationQuery);
   }
 }
